64fe0a0fdd2995ba6802aac4a1b04a122fd34f5f
Kacper Łukawski <ka.lukawski@gmail.com>
2017-02-10 23:16:57 +0100

    Getting user system, not sure about ip

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const app = express()
 
 app.get('/', (req, res) => {
   console.log(req.headers['user-agent'])
+  console.log(req.headers['X-Forwarded-For'])
 
-  var ip = req.ip
+  var system = req.headers['user-agent'].substring(req.headers['user-agent'].indexOf('(') + 1, req.headers['user-agent'].indexOf(')'))
+  var ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress).split(",")[0];
+  ip = ip.replace('::ffff:', '')
   var languages = req.acceptsLanguages()
+
   var responseObject = {
     language: languages[0],
-    ip: ip
+    ip: ip,
+    system: system
   }
   res.send(responseObject)
 })
