9c0fe5fc3dd795b2c78f365d355b8bb035b8bcf5
Kacper Łukawski <ka.lukawski@gmail.com>
2017-02-10 23:05:46 +0100

    Getting user language

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) {
-  console.log(req.ip)
-  res.end()
+app.get('/', (req, res) => {
+  console.log(req.headers['user-agent'])
+
+  var ip = req.ip
+  var languages = req.acceptsLanguages()
+  var responseObject = {
+    language: languages[0],
+    ip: ip
+  }
+  res.send(responseObject)
 })
 
 app.listen(process.env.PORT || 3000)
